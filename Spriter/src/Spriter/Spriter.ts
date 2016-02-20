8e55ffeca5ef1316861d16e81b2715bbe3ad4db9
Tomas Rychnovsky <tomas.rychnovsky@gmail.com>
2016-02-20 21:44:10 +0100

    Added Pro features, lot of big changes

--- Spriter/src/Spriter/Spriter.ts
+++ Spriter/src/Spriter/Spriter.ts
@@ -2,43 +2,65 @@
 
     export class Spriter {
 
-        private _folders: Helper.IdNameMap<Folder>;
-        private _entities: Helper.IdNameMap<Entity>;
+        private _folders: IdNameMap<Folder>;
+        private _tags: IdNameMap<Item>;
+        private _entities: IdNameMap<Entity>;
 
         // -------------------------------------------------------------------------
         constructor() {
-            this._folders = new Helper.IdNameMap<Folder>();
-            this._entities = new Helper.IdNameMap<Entity>();
+            this._folders = new IdNameMap<Folder>();
+            this._tags = new IdNameMap<Item>();
+            this._entities = new IdNameMap<Entity>();
         }
 
         // -------------------------------------------------------------------------
-        public addFolder(aFolder: Folder): void {
-            this._folders.add(aFolder, aFolder.id, aFolder.name);
+        public addFolder(folder: Folder): void {
+            this._folders.add(folder, folder.id, folder.name);
         }
 
         // -------------------------------------------------------------------------
-        public getFolderById(aId: number): Folder {
-            return this._folders.getById(aId);
+        public getFolderById(id: number): Folder {
+            return this._folders.getById(id);
         }
 
         // -------------------------------------------------------------------------
-        public getFolderByName(aName: string): Folder {
-            return this._folders.getByName(aName);
+        public getFolderByName(name: string): Folder {
+            return this._folders.getByName(name);
         }
 
         // -------------------------------------------------------------------------
-        public addEntity(aEntitiy: Entity): void {
-            this._entities.add(aEntitiy, aEntitiy.id, aEntitiy.name);
+        public addEntity(entity: Entity): void {
+            this._entities.add(entity, entity.id, entity.name);
         }
 
         // -------------------------------------------------------------------------
-        public getEntityById(aId: number): Entity {
-            return this._entities.getById(aId);
+        public getEntityById(id: number): Entity {
+            return this._entities.getById(id);
         }
 
         // -------------------------------------------------------------------------
-        public getEntityByName(aName: string): Entity {
-            return this._entities.getByName(aName);
+        public getEntityByName(name: string): Entity {
+            return this._entities.getByName(name);
+        }
+
+        // -------------------------------------------------------------------------
+        public addTag(tag: Item): void {
+            this._tags.add(tag, tag.id, tag.name);
+        }
+
+        // -------------------------------------------------------------------------
+        public getTagById(id: number): Item {
+            return this._tags.getById(id);
+        }
+
+        // -------------------------------------------------------------------------
+        public getTagByName(name: string): Item {
+            return this._tags.getByName(name);
+        }
+
+        // -------------------------------------------------------------------------
+        public get tagsLength(): number {
+            return this._tags.length;
         }
     }
 }
