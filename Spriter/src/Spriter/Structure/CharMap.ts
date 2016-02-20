ea39959406b6f92fefb311596bb53007fcd4d8e5
Tomas Rychnovsky <tomas.rychnovsky@gmail.com>
2016-02-20 22:58:13 +0100

    Missing CharMap added

---
+++ Spriter/src/Spriter/Structure/CharMap.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="Item.ts" />
+
+module Spriter {
+
+    export class CharMap extends Item {
+
+        private _map: any;
+
+        // -------------------------------------------------------------------------
+        public put(key: string, value: File): void {
+            if (this._map === undefined) {
+                this._map = {}
+            }
+
+            if (this._map[key] !== undefined) {
+                console.error("Key with name " + key + " already exists");
+            }
+
+            this._map[key] = value;
+        }
+
+        // -------------------------------------------------------------------------
+        public value(key: string): File {
+            return this._map[key];
+        }
+    }
+}
